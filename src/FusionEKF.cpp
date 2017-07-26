7b4595f6897304663d824a3ad2ed9de39ea12c3d
Kyle Rector <kylearector@outlook.com>
2017-07-26 19:20:05 -0400

    feat: Filled out FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -26,18 +26,36 @@ FusionEKF::FusionEKF() {
   R_laser_ << 0.0225, 0,
               0, 0.0225;
 
+  // Measurement noise covariance matrix
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
+
   // Measurement covariance matrix - radar
   R_radar_ << 0.09, 0, 0,
               0, 0.0009, 0,
               0, 0, 0.09;
 
-  /**
-  TODO:
-    * Finish initializing the FusionEKF.
-    * Set the process and measurement noises
-  */
+  VectorXd x_(4);
+  x_ << 1, 1, 1, 1;
 
+  // State covariance matrix
+  MatrixXd P_ = MatrixXd(4, 4);
+  P_ << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1000, 0,
+        0, 0, 0, 1000;
 
+  // Transition matrix
+  MatrixXd F_ = MatrixXd(4, 4);
+  F_ << 1, 0, 1, 0,
+			  0, 1, 0, 1,
+			  0, 0, 1, 0,
+			  0, 0, 0, 1;
+
+// Process covariance matrix
+  MatrixXd Q_ = MatrixXd(4, 4);
+
+  ekf_.Init(x_, P_, F_, H_laser_, R_laser_, Q_);
 }
 
 /**
@@ -64,15 +82,24 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      /**
-      Convert radar from polar to cartesian coordinates and initialize state.
-      */
+      // Convert polar coordinates to x and y
+      // X
+      ekf_.x_[0] = measurement_pack.raw_measurements_[0];
+      // Y
+      ekf_.x_[1] = measurement_pack.raw_measurements_[1];
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-      Initialize state.
-      */
+      // If type laser, data is already in x and y
+      // X
+      ekf_.x_[0] = measurement_pack.raw_measurements_[0];
+      // Y
+      ekf_.x_[1] = measurement_pack.raw_measurements_[1];
     }
+
+    // Set the previous timestamp to the current from the measurement pack
+    // Ensures that no extra time is elapsed between initialization and
+    // next steps
+    previous_timestamp_ = measurement_pack.timestamp_;
 
     // Done initializing, no need to predict or update
     is_initialized_ = true;
@@ -83,13 +110,28 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Prediction
    ****************************************************************************/
 
-  /**
-   TODO:
-     * Update the state transition matrix F according to the new elapsed time.
-      - Time is measured in seconds.
-     * Update the process noise covariance matrix.
-     * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
+  // calculate delta t in seconds
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+
+  // Update the timestamp
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  // Update the transition matrix with time delta
+  ekf_.F_(0,2) = dt;
+  ekf_.F_(1,3) = dt;
+
+  // Update process modeling noise
+  float dt_2 = dt* dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  float noise_ax = 9;
+  float noise_ay = 9;
+
+  ekf_.Q_ << dt_4 * noise_ax/4, 0, dt_3 * noise_ax/2, 0,
+	           0, dt_4 * noise_ay/4, 0, dt_3 * noise_ay/2,
+	           dt_3 * noise_ax/2, 0, dt_2 * noise_ax, 0,
+             0, dt_3 * noise_ay/2, 0, dt_2 * noise_ay;
 
   ekf_.Predict();
 
@@ -97,16 +139,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Update
    ****************************************************************************/
 
-  /**
-   TODO:
-     * Use the sensor type to perform the update step.
-     * Update the state and covariance matrices.
-   */
-
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-  } else {
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+  }
+  else {
     // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
